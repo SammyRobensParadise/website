d0affb79a935ae89a92722447602e1c94b847040
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-29 15:04:28 -0700

    got latest version of eunoia

--- website-container/src/App.js
+++ website-container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'eunoia';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Test</Button>
       </header>
     </div>
   );
