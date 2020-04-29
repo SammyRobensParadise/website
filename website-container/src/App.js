222e49b0219d35d168b6054384bed7ef57c21479
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-29 15:09:36 -0700

    cleaned up app entiry point

--- website-container/src/App.js
+++ website-container/src/App.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'eunoia';
+import React from 'react'
+import './App.css'
+import { Button } from 'eunoia'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Test</Button>
-      </header>
+      <Button>Test</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
