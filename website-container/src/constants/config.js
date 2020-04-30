3349529959cad24e422a9c7a1b47480222f0bac5
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-04-29 18:26:03 -0700

    changed locale of menu config

--- website-container/src/constants/config.js
+++ website-container/src/constants/config.js
@@ -1,16 +1,53 @@
+/**
+ * `constants`
+ */
+
+ /**
+  * UI Style config
+  */
 export const UIStyle = {
-    UIColors: {
-      white: '#FFFFFF',
-      lightGrey: '#00000029',
-      black: '#000000',
-      gold: '#E1A87A',
-      darkGrey: '#212121',
-      mediumGrey: '#5D5D5D',
+  UIColors: {
+    white: '#FFFFFF',
+    lightGrey: '#00000029',
+    black: '#000000',
+    gold: '#E1A87A',
+    darkGrey: '#212121',
+    mediumGrey: '#5D5D5D',
+  },
+  Shadows: {
+    boxShadow: '0px 6px 6px #00000029',
+    boxShadowDeep: '0px 12px 1px #00000029',
+    boxShadowImage: '0px 12px 12px #00000029',
+  },
+}
+
+/**
+ * Menu Config
+ */
+export const MenuConfig = {
+  config: {
+    fontColor: UIStyle.UIColors.white,
+    activeSection: 'Home',
+    hideOnMobile: true,
+    DisableMobile: false,
+    fontOverride: 'Helvetica Neue',
+    boldText: false,
+    spacing: 0,
+    row: true,
+    mobileBreakpoint: 850,
+    breakpoints: { xs: true },
+  },
+  options: [
+    {
+      title: 'Home',
+      link: '/#',
+      newTab: false,
+      main: true,
     },
-    Shadows: {
-      boxShadow: '0px 6px 6px #00000029',
-      boxShadowDeep: '0px 12px 1px #00000029',
-      boxShadowImage: '0px 12px 12px #00000029',
-    },
-  }
-  
+    { title: 'About', link: '/#', newTab: false },
+    { title: 'Articles', link: '/#', newTab: false },
+    { title: 'Design', link: '/#', newTab: false },
+    { title: 'Development', link: '/#', newTab: false },
+    { title: 'Experience', link: '/#', newTab: false },
+  ],
+}
--- website-container/src/pages/Main.js
+++ website-container/src/pages/Main.js
@@ -2,35 +2,10 @@ import React from 'react'
 import '../css/styles/App.css'
 import { Cover } from '../components/Cover'
 import { Menu } from 'eunoia'
-import { UIStyle } from '../constants/config'
-const config = {
-  fontColor: UIStyle.UIColors.white,
-  activeSection: 'Home',
-  hideOnMobile: true,
-  DisableMobile: false,
-  fontOverride: 'Helvetica Neue',
-  boldText: false,
-  spacing: 0,
-  row: true,
-  mobileBreakpoint: 850,
-  breakpoints: { xs: true },
-}
-const options = [
-  {
-    title: 'Home',
-    link: '/#',
-    newTab: false,
-    main: true,
-  },
-  { title: 'About', link: '/#', newTab: false },
-  { title: 'Articles', link: '/#', newTab: false },
-  { title: 'Design', link: '/#', newTab: false },
-  { title: 'Development', link: '/#', newTab: false },
-  { title: 'Experience', link: '/#', newTab: false },
-]
-const AppMenu = <Menu config={config} options={options}  />
-class Main extends React.PureComponent {
+import { MenuConfig } from '../constants/config'
 
+const AppMenu = <Menu config={MenuConfig.config} options={MenuConfig.options} />
+class Main extends React.PureComponent {
   render() {
     return (
       <div>
