ac834d3f952b12af759d238d85da8ca119f99faa
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-05-03 16:42:53 -0700

    added tool section

--- website-container/src/components/Tools.jsx
+++ website-container/src/components/Tools.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Container } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { UIStyle } from '../constants/config'
 import styled from 'styled-components'
@@ -17,9 +17,19 @@ const Wrapper = styled.div`
     display: block;
   }
 `
-
+const NodeContainer = styled.div`
+text-align: center;
+  margin: 0 auto;
+  display: block;
+  padding-block-end: 6vh;
+`
+/**
+ *
+ * @param {Array} tools
+ */
 const Tools = ({ tools }) => {
   const classes = useStyles()
+  //create lists
   let col0 = []
   let col1 = []
   let col2 = []
@@ -46,30 +56,32 @@ const Tools = ({ tools }) => {
   }
   return (
     <Wrapper>
+        <Container>
       <div className={classes.root}>
         <Grid container>
-          <Grid item xs={6} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3}>
             {col0.map((it) => (
-              <div>{it}</div>
+              <NodeContainer>{it}</NodeContainer>
             ))}
           </Grid>
-          <Grid item xs={6} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3}>
             {col1.map((it) => (
-              <div>{it}</div>
+              <NodeContainer>{it}</NodeContainer>
             ))}
           </Grid>
-          <Grid item xs={6} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3}>
             {col2.map((it) => (
-              <div>{it}</div>
+              <NodeContainer>{it}</NodeContainer>
             ))}
           </Grid>
-          <Grid item xs={6} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3}>
             {col3.map((it) => (
-              <div>{it}</div>
+              <NodeContainer>{it}</NodeContainer>
             ))}
           </Grid>
         </Grid>
       </div>
+      </Container>
     </Wrapper>
   )
 }
