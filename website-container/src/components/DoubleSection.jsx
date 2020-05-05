5fb3c1f21e8a3de8c016c8e9cb3f0d7114e64e11
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-05-04 23:32:21 -0700

    added DoubleOffset component

---
+++ website-container/src/components/DoubleSection.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { UIStyle } from '../constants/config'
+import styled from 'styled-components'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: UIStyle.UIColors.white,
+  },
+}))
+
+const Wrapper = styled.div`
+  padding-block-start: 10vh;
+  @media screen and (max-width: 600px) {
+    display: block;
+  }
+`
+
+const DoubleOffsetSection = ({ leftChildren, rightChildren, reverse }) => {
+  const classes = useStyles()
+  return (
+    <Wrapper>
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={reverse ? 4 : 8}>
+            {leftChildren
+              ? leftChildren.map((children) => <div className="-left-children">{children}</div>)
+              : null}
+          </Grid>
+          <Grid item xs={12} sm={12} md={reverse ? 8 : 4}>
+            {rightChildren
+              ? rightChildren.map((children) => <div className="-right-children">{children}</div>)
+              : null}
+          </Grid>
+        </Grid>
+      </div>
+    </Wrapper>
+  )
+}
+
+export { DoubleOffsetSection }
