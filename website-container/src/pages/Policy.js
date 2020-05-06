23d7d538b04f8e6650564d45a818d48626cb8c55
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-05-05 21:36:56 -0700

    fixed typos in  prvacy policy

--- website-container/src/pages/Policy.js
+++ website-container/src/pages/Policy.js
@@ -70,23 +70,23 @@ const CoverChildren = (
 // cards
 const PolicyWriteup = (
   <PolicyText>
-    <h1>Welcome to our Privacy Policy</h1>
+    <h1>Welcome tomyPrivacy Policy</h1>
     <h3>Your privacy is critically important to us.</h3>
     <p>
       It is Sammy Robens-Paradise's policy to respect your privacy regarding any information we may
-      collect while operating our website. This Privacy Policy applies to
+      collect while operatingmywebsite. This Privacy Policy applies to
       <a href="https://sammy.world"> https://sammy.world</a> (hereinafter, "us", "we", or
       "https://sammy.world"). We respect your privacy and are committed to protecting personally
       identifiable information you may provide us through the Website. We have adopted this privacy
-      policy ("Privacy Policy") to explain what information may be collected on our Website, how we
+      policy ("Privacy Policy") to explain what information may be collected onmyWebsite, how we
       use this information, and under what circumstances we may disclose the information to third
       parties. This Privacy Policy applies only to information we collect through the Website and
-      does not apply to our collection of information from other sources.
+      does not apply tomycollection of information from other sources.
     </p>
     <p>
-      This Privacy Policy, together with the Terms and conditions posted on our Website, set forth
-      the general rules and policies governing your use of our Website. Depending on your activities
-      when visiting our Website, you may be required to agree to additional terms and conditions.
+      This Privacy Policy, together with the Terms and conditions posted onmyWebsite, set forth
+      the general rules and policies governing your use ofmyWebsite. Depending on your activities
+      when visitingmyWebsite, you may be required to agree to additional terms and conditions.
     </p>
     <h2>Cookies</h2>
     <p>
@@ -105,7 +105,7 @@ const PolicyWriteup = (
       of cookies.
     </p>
     <p>
-      By continuing to navigate our website without changing your cookie settings, you hereby
+      By continuing to navigatemywebsite without changing your cookie settings, you hereby
       acknowledge and agree to Sammy Robens-Paradise's use of cookies.
     </p>
     <h2>{policySubtitle}</h2>
