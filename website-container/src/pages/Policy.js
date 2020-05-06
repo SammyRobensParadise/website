f434d708be8c0e990c2966c8cbb3b63a826dbe91
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-05-05 21:40:47 -0700

    some more quick changes

--- website-container/src/pages/Policy.js
+++ website-container/src/pages/Policy.js
@@ -18,7 +18,7 @@ import '../css/styles/App.css'
 // strings ****
 let d = new Date()
 const policy = `Policy`
-const policySubtitle = `© ${d.getFullYear()} Sammy Robens-Paradise`
+const policySubtitle = `© ${d.getFullYear()} Sam my  Robens-Paradise`
 const learnMore = `Policy`
 
 // components
@@ -70,43 +70,43 @@ const CoverChildren = (
 // cards
 const PolicyWriteup = (
   <PolicyText>
-    <h1>Welcome tomyPrivacy Policy</h1>
+    <h1>Welcome to my Privacy Policy</h1>
     <h3>Your privacy is critically important to us.</h3>
     <p>
-      It is Sammy Robens-Paradise's policy to respect your privacy regarding any information we may
-      collect while operatingmywebsite. This Privacy Policy applies to
+      It is Sam my  Robens-Paradise's policy to respect your privacy regarding any information we may
+      collect while operating my website. This Privacy Policy applies to
       <a href={window.origin}> {window.origin}</a> (hereinafter, "us", "we", or
-      "https://sammy.world"). We respect your privacy and are committed to protecting personally
+      "https://sam my .world"). We respect your privacy and are committed to protecting personally
       identifiable information you may provide us through the Website. We have adopted this privacy
-      policy ("Privacy Policy") to explain what information may be collected onmyWebsite, how we
+      policy ("Privacy Policy") to explain what information may be collected on my Website, how we
       use this information, and under what circumstances we may disclose the information to third
       parties. This Privacy Policy applies only to information we collect through the Website and
-      does not apply tomycollection of information from other sources.
+      does not apply to my collection of information from other sources.
     </p>
     <p>
-      This Privacy Policy, together with the Terms and conditions posted onmyWebsite, set forth
-      the general rules and policies governing your use ofmyWebsite. Depending on your activities
-      when visitingmyWebsite, you may be required to agree to additional terms and conditions.
+      This Privacy Policy, together with the Terms and conditions posted on my Website, set forth
+      the general rules and policies governing your use of  my  Website. Depending on your activities
+      when visiting my Website, you may be required to agree to additional terms and conditions.
     </p>
     <h2>Cookies</h2>
     <p>
-      To enrich and perfect your online experience, Sammy Robens-Paradise uses "Cookies", similar
+      To enrich and perfect your online experience, Sam my  Robens-Paradise uses "Cookies", similar
       technologies and services provided by others to display personalized content, appropriate
       advertising and store your preferences on your computer.
     </p>
     <p>
       A cookie is a string of information that a website stores on a visitor's computer, and that
-      the visitor's browser provides to the website each time the visitor returns. Sammy
-      Robens-Paradise uses cookies to help Sammy Robens-Paradise identify and track visitors, their
-      usage of https://sammy.world, and their website access preferences. Sammy Robens-Paradise
+      the visitor's browser provides to the website each time the visitor returns. Sam my 
+      Robens-Paradise uses cookies to help Sam my  Robens-Paradise identify and track visitors, their
+      usage of https://sam my .world, and their website access preferences. Sam my  Robens-Paradise
       visitors who do not wish to have cookies placed on their computers should set their browsers
-      to refuse cookies before using Sammy Robens-Paradise's websites, with the drawback that
-      certain features of Sammy Robens-Paradise's websites may not function properly without the aid
+      to refuse cookies before using Sam my  Robens-Paradise's websites, with the drawback that
+      certain features of Sam my  Robens-Paradise's websites may not function properly without the aid
       of cookies.
     </p>
     <p>
-      By continuing to navigatemywebsite without changing your cookie settings, you hereby
-      acknowledge and agree to Sammy Robens-Paradise's use of cookies.
+      By continuing to navigate my website without changing your cookie settings, you hereby
+      acknowledge and agree to Sam my  Robens-Paradise's use of cookies.
     </p>
     <h2>{policySubtitle}</h2>
     <p>Thank you.</p>
