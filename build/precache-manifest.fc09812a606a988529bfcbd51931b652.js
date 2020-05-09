386e6228712b49da70d80cf1ffec27c64c56bc87
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-05-08 20:55:45 -0700

    added build

---
+++ build/precache-manifest.fc09812a606a988529bfcbd51931b652.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d04c0e8bcbf9d96a2a20f4ef92b2e3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2138343b6bf6bc8315f",
+    "url": "/static/css/main.4cb89b6c.chunk.css"
+  },
+  {
+    "revision": "ec37861a2fa0c559f41e",
+    "url": "/static/js/2.de18b028.chunk.js"
+  },
+  {
+    "revision": "e85e620f3fcc49b161ee1b95d282124d",
+    "url": "/static/js/2.de18b028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2138343b6bf6bc8315f",
+    "url": "/static/js/main.ef29da40.chunk.js"
+  },
+  {
+    "revision": "3875548b154181aee6f0",
+    "url": "/static/js/runtime-main.107de957.js"
+  },
+  {
+    "revision": "7f281199258d96e249a7fce4101006b9",
+    "url": "/static/media/Helvetica Neu Bold.7f281199.ttf"
+  },
+  {
+    "revision": "4ff686ee78ff095848014f4283f67a41",
+    "url": "/static/media/HelveticaNeue BlackCond.4ff686ee.ttf"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/static/media/HelveticaNeue Light.0facaae9.ttf"
+  },
+  {
+    "revision": "0a13c540938b1b7dd3996b02ea568e5f",
+    "url": "/static/media/HelveticaNeue Medium.0a13c540.ttf"
+  },
+  {
+    "revision": "78c28465643a20597ce65eee037a7675",
+    "url": "/static/media/HelveticaNeue Thin.78c28465.ttf"
+  },
+  {
+    "revision": "ccad04d46768981ff847f22e8a53b5b3",
+    "url": "/static/media/HelveticaNeue.ccad04d4.ttf"
+  },
+  {
+    "revision": "b09282d354788cf70d0fd3cfd49e4af2",
+    "url": "/static/media/georgia.b09282d3.woff"
+  },
+  {
+    "revision": "8a8c360b69d19bcc62c8170a8cd24efd",
+    "url": "/static/media/georgiab.8a8c360b.woff"
+  },
+  {
+    "revision": "aaf84cf4a332a4e0e2817b8ead6647f1",
+    "url": "/static/media/georgiai.aaf84cf4.woff"
+  },
+  {
+    "revision": "2fccff89f02505407708c0055768a640",
+    "url": "/static/media/georgiaz.2fccff89.woff"
+  }
+]);
