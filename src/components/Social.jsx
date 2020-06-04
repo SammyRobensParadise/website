9f9e16ef6db0aaceadbb9c0f4d7d32dcf9af9a59
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-06-03 22:22:25 -0700

    added tooltips to social media links fo social

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -29,20 +29,47 @@ const SocialMediaButtons = styled.a`
       filter: ${`drop-shadow(2px 2px 5px ${UIStyle.UIColors.black})`};
     }
   }
+  :hover,
+  :focus {
+    .tooltip {
+      opacity: 1;
+    }
+  }
+`
+const SocialToolTip = styled.div`
+  opacity: 0;
+  height: 30px;
+  font-size: 16px;
+  font-family: Helvetica Neue;
+  letter-spacing: 0px;
+  line-height: 30px;
+  background-color: ${UIStyle.UIColors.mediumGrey};
+  color: ${UIStyle.UIColors.white};
+  width: auto;
+  position: fixed;
+  right: 100px;
+  padding: 5px 10px 5px 10px;
+  border-radius: 8px;
+  box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.75);
+  transition: all 0.5s ease 0.3s;
 `
 const SocialButtons = () => {
   return (
     <SocialButtonsWrapper>
       <SocialMediaButtons aria-label="dribbble" href={dribbbleURL}>
+        <SocialToolTip className="tooltip">Dribbble</SocialToolTip>
         <DribbbleIcon color={UIStyle.UIColors.white} scalingFactor={1} />
       </SocialMediaButtons>
       <SocialMediaButtons aria-label="github" href={githubURL}>
+        <SocialToolTip className="tooltip">Github</SocialToolTip>
         <GithubIcon color={UIStyle.UIColors.white} scalingFactor={1} />
       </SocialMediaButtons>
       <SocialMediaButtons aria-label="medium" href={mediumURL}>
+        <SocialToolTip className="tooltip">Medium</SocialToolTip>
         <MediumIcon color={UIStyle.UIColors.white} scalingFactor={1} />
       </SocialMediaButtons>
       <SocialMediaButtons aria-label="linkedin" href={linkedinURL}>
+        <SocialToolTip className="tooltip">Linkedin</SocialToolTip>
         <LinkedinIcon color={UIStyle.UIColors.white} scalingFactor={1} />
       </SocialMediaButtons>
     </SocialButtonsWrapper>
@@ -53,15 +80,19 @@ const StorySocialButtons = () => {
   return (
     <SocialButtonsWrapper>
       <SocialMediaButtons aria-label="dribbble" href={dribbbleURL}>
+        <SocialToolTip className="tooltip">Dribbble</SocialToolTip>
         <DribbbleIcon color={UIStyle.UIColors.black} scalingFactor={1} />
       </SocialMediaButtons>
       <SocialMediaButtons aria-label="github" href={githubURL}>
+        <SocialToolTip className="tooltip">Github</SocialToolTip>
         <GithubIcon color={UIStyle.UIColors.black} scalingFactor={1} />
       </SocialMediaButtons>
       <SocialMediaButtons aria-label="medium" href={mediumURL}>
+        <SocialToolTip className="tooltip">Medium</SocialToolTip>
         <MediumIcon color={UIStyle.UIColors.black} scalingFactor={1} />
       </SocialMediaButtons>
       <SocialMediaButtons aria-label="linkedin" href={linkedinURL}>
+        <SocialToolTip className="tooltip">Linkedin</SocialToolTip>
         <LinkedinIcon color={UIStyle.UIColors.black} scalingFactor={1} />
       </SocialMediaButtons>
     </SocialButtonsWrapper>
